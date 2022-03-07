3f6dc42172f0e0e4134f73196687c34712a60f8d
lokeshvangara <99350727+lokeshvangara@users.noreply.github.com>
2022-03-07 15:12:48 +0530

    Create primitive datatypes.js

---
+++ primitive datatypes.js
@@ -0,0 +1,22 @@
+//primitive datatypes
+/** In JavaScript, a primitive (primitive value, primitive data type) is data that is not an object and has no methods. */
+
+//string
+let name = "lokesh";
+console.log( typeof name);
+
+//number
+let age = 23;
+console.log(typeof age);
+
+//boolean
+let isApproved = true;
+console.log(typeof isApproved);
+
+//undefined
+let firstName = undefined;
+console.log( firstName);
+
+//null
+let lastName = null;
+console.log( lastName);
